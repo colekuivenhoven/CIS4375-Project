e60e75df09edc1218328211d83bd5ba05d3519e9
colekuivenhoven <villageofcrystals@gmail.com>
2021-09-11 20:08:00 -0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+// All imports must be done before the main function
+  // Importing main CSS file. This CSS file can be used in other pages without importing it in each file.
+import './assets/styles/App.css';
 
+  // Importing 'pages' that will be used
+import Home from './pages/Home.js';
+import AdminHome from './pages/AdminHome.js';
+import Account from './pages/Account.js';
+
+  // Importing the router files
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+// Root application function
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // Root 'Router' element for switching between pages. The return can have only one root element.
+    <Router>
+      <div>
+        <nav className="app-navigation" id="navbar">
+          {/* Visible link elements to different pages */}
+          <Link className="route-link" to="/">Home</Link>
+          <Link className="route-link" to="/account">Account</Link>
+          <Link className="route-link" to="/admin">Admin</Link>
+        </nav>
+
+        {/* Route path definitions */}
+        <Switch>
+          <Route path="/admin">
+            <AdminHome />
+          </Route>
+          <Route path="/account">
+            <Account />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
+// Function must be 'exposed' to rest of the application at the end of the file as shown below.
 export default App;
---
+++ src/pages/Account.js
@@ -0,0 +1,68 @@
+// All imports must be done before the main function
+    // Importing this page's CSS file
+import '../assets/styles/Account.css';
+
+// Importing common files used in react
+import React, { useEffect, useRef, useState } from "react";
+
+// Variables declared that will persist even if page is changed
+var localNumberRaw = 0;
+
+// Main function for the specific 'page'
+function Account() {
+    // 'Reactive' variables that will cause the page to update when their values change
+        // 'useState' at the end of each describes their initial value
+    const [localNumber, setLocalNumber] = useState(localNumberRaw)
+        // The initial value below uses a conditional operator which means: 
+            // if (condition) then (value) else (other_value)
+            // Turns into...
+            // (condition) ? (value) : (other_value)
+    const [globalNumber, setGlobalNumber] = useState(window.sessionStorage.getItem("num_global") ? window.sessionStorage.getItem("num_global") : 0)
+
+    // Regular varaible declaration
+    const pageTitle = "Account"
+
+    // Handling functions
+    function handleAddButtonLocal() {
+        localNumberRaw += 1;
+        setLocalNumber(localNumberRaw);
+    }
+
+    function handleAddButtonGlobal() {
+        setGlobalNumber(parseInt(globalNumber)+1);
+        window.sessionStorage.setItem("num_global", parseInt(globalNumber)+1);
+    }
+
+    return (
+        // Empty root element. The return can have only one root element
+        <>
+            <div className="container-account">
+                {/* Variables can be inserted inside of brackets as shown below */}
+                <div className="page-title"><span className="font-round-large">{pageTitle}</span></div>
+                <div className="test-container">
+                    <span className="font-round-medium unselectable">Local Number: {localNumber}</span>
+                    <div 
+                        className="container-add unselectable" 
+                        onClick={() => {
+                            // Calling the appropriate handle function when the element has been clicked.
+                            handleAddButtonLocal();
+                        }}
+                    ></div>
+                </div>
+                <div className="test-container">
+                    <span className="font-round-medium unselectable">Global Number: {globalNumber}</span>
+                    <div 
+                        className="container-add unselectable" 
+                        onClick={() => {
+                            // Calling the appropriate handle function when the element has been clicked.
+                            handleAddButtonGlobal();
+                        }}
+                    ></div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+// Function must be 'exposed' to rest of the application at the end of the file as shown below.
+export default Account;
