f99243a37729e4bd0302fa9ad145cf168c0b7e19
colekuivenhoven <villageofcrystals@gmail.com>
2021-09-21 17:54:01 -0500

    Added test function to connect to AWS server

--- src/pages/Test.js
+++ src/pages/Test.js
@@ -8,8 +8,13 @@ import React, { useEffect, useRef, useState } from "react";
 // Variables declared that will persist even if page is changed
 var localNumberRaw = 0;
 
+var testDataRaw = [];
+var testDataConverted = [];
+var gotTestData = false;
+
 // Main function for the specific 'page'
 function Test(props) {
+    const[testData, setTestData] = useState([])
 
     // Regular varaible declaration
     const pageTitle = "Testing Page"
@@ -17,6 +22,10 @@ function Test(props) {
 
     // 'useEffect' runs once for every render of the page
     useEffect(() => {
+        if(!gotTestData) {
+            getTestData();
+        }
+
         var mainContainer = document.querySelector(".container-test");
         var fontLarge = document.querySelectorAll(".font-round-large");
         var fontMed = document.querySelectorAll(".font-round-medium");
@@ -55,6 +64,32 @@ function Test(props) {
         }
     });
 
+    // Get data functions
+    async function getTestData() {
+        let response = await fetch("http://3.218.225.62:3040/read");
+        response = await response.json();
+        testDataRaw = response.visitors;
+
+        if (!gotTestData) {
+            convertTestData();
+        }
+    }
+
+    function convertTestData() {
+        testDataRaw.forEach(visitor => {
+            testDataConverted.push(
+                <div key={visitor._id} style={{display: 'flex', flexDirection: 'column', marginTop: '1vmin'}}>
+                    <b>Name: {visitor.name}</b>
+                    <span>Connect Date: {visitor.date}</span>
+                </div>
+            )
+        });
+
+        setTestData(testDataConverted);
+        gotTestData = true;
+    }
+
+
     // Handling functions
     
 
@@ -66,12 +101,12 @@ function Test(props) {
                 <div className="page-title"><span className="font-round-large">{pageTitle}</span></div>
                 <div className="test-form-container">
                     <div className="test-object-container">
-                        <span>Test Item</span>
+                        <div>{testData}</div>
                     </div>
                 </div>
             </div>
             <div className="test-modal">
-
+                
             </div>
         </>
     )
