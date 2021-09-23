7e1c393842b8f4ae9cb387d992554e478ee6c247
colekuivenhoven <villageofcrystals@gmail.com>
2021-09-23 15:44:03 -0500

    Added connection functionality to MySQL database

--- src/pages/Test.js
+++ src/pages/Test.js
@@ -15,6 +15,10 @@ var gotTestData = false;
 // Main function for the specific 'page'
 function Test(props) {
     const[testData, setTestData] = useState([]);
+    const[testUsername, setTestUsername] = useState('');
+    const[testPassword, setTestPassword] = useState('');
+    const[testPhone, setTestPhone] = useState('');
+    const[testEmail, setTestEmail] = useState('');
 
     // Regular varaible declaration
     const pageTitle = "Testing Page"
@@ -70,9 +74,9 @@ function Test(props) {
 
     // Get data functions
     async function getTestData() {
-        let response = await fetch("http://3.218.225.62:3040/read");
+        let response = await fetch("http://3.218.225.62:3040/test/mysql");
         response = await response.json();
-        testDataRaw = response.visitors;
+        testDataRaw = response.users.reverse();
         testDataConverted = [];
 
         if (!gotTestData) {
@@ -81,11 +85,16 @@ function Test(props) {
     }
 
     function convertTestData() {
-        testDataRaw.forEach(visitor => {
+        testDataRaw.forEach((user,index) => {
             testDataConverted.push(
-                <div key={visitor._id} style={{display: 'flex', flexDirection: 'column', marginTop: '1vmin'}}>
-                    <b>Name: {visitor.name}</b>
-                    <span>Connect Date: {visitor.date}</span>
+                <div 
+                    key={index} 
+                    className="test-user-container"
+                >
+                    <div className="test-user-item">{user.username}</div>
+                    <div className="test-user-item">{user.password}</div>
+                    <div className="test-user-item">{user.phone}</div>
+                    <div className="test-user-item">{user.email}</div>
                 </div>
             )
         });
@@ -94,9 +103,43 @@ function Test(props) {
         gotTestData = true;
     }
 
+    function addUser(data) {
+        fetch("http://3.218.225.62:3040/users/add", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data)
+        })
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .then(() => {
+            gotTestData = false;
+            getTestData();
+        })
+    }
 
     // Handling functions
-    
+    function handleSubmit() {
+        var data = {
+            username: testUsername,
+            password: testPassword,
+            phone: testPhone,
+            email: testEmail
+        }
+
+        if(data.username == '' || data.password == '' || data.phone == '' || data.email == '') {
+            console.log("Empty Field!")
+        }
+        else {
+            addUser(data);
+        }
+
+        setTestUsername('');
+        setTestPassword('');
+        setTestPhone('');
+        setTestEmail('');
+    }
 
     return (
         // Empty root element. The return can have only one root element
@@ -105,9 +148,35 @@ function Test(props) {
                 {/* Variables can be inserted inside of brackets as shown below */}
                 <div className="page-title"><span className="font-round-large">{pageTitle}</span></div>
                 <div className="test-form-container">
-                    <div className="test-object-container">
-                        <div>{testData}</div>
+                    <div className="test-object-header">
+                        <div className="test-object-header-item">
+                            Username
+                        </div>
+                        <div className="test-object-header-item">
+                            Password
+                        </div>
+                        <div className="test-object-header-item">
+                            Phone #
+                        </div>
+                        <div className="test-object-header-item">
+                            Email
+                        </div>
                     </div>
+                    <div className="test-object-container">
+                        
+                        <div style={{display: 'flex', flexDirection: 'row', flexWrap: 'wrap', transition: 'all 0.25s linear'}}>{testData}</div>
+                    </div>
+                    <div className="test-add-container">
+                        <b style={{color: 'rgb(255, 214, 110)', marginLeft: 'auto', fontSize: '2vmin', marginBottom: '1vmin'}}>Add User</b>
+                        <span className="test-add-text">Username: <input className="test-add-input" value={testUsername} onChange={(e) => {setTestUsername(e.target.value)}}/></span>
+                        <span className="test-add-text">Password: <input className="test-add-input" value={testPassword} onChange={(e) => {setTestPassword(e.target.value)}}/></span>
+                        <span className="test-add-text">Phone: <input className="test-add-input" value={testPhone} onChange={(e) => {setTestPhone(e.target.value)}}/></span>
+                        <span className="test-add-text">Email: <input className="test-add-input" value={testEmail} onChange={(e) => {setTestEmail(e.target.value)}}/></span>
+                        <button className="test-add-button" onClick={() => {handleSubmit()}}>+ Add</button>
+                    </div>
+                </div>
+                <div className="test-object-info-container">
+
                 </div>
             </div>
             <div className="test-modal">
