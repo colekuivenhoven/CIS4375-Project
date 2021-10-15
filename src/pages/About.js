aff218fe1bc79d452aced04c34fc08d2342f0a09
William Huff <huff.sterling@gmail.com>
2021-10-15 12:02:40 -0500

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,102 +1,104 @@
 // All imports must be done before the main function
     // Importing this page's CSS file
-import '../assets/styles/About.css';
+    import '../assets/styles/About.css';
 
-// Importing common files used in react
-import React, { useEffect, useRef, useState } from "react";
-
-// Variables declared that will persist even if page is changed
-var localNumberRaw = 0;
-
-// Main function for the specific 'page'
-function About(props) {
-    // 'Reactive' variables that will cause the page to update when their values change
-        // 'useState' at the end of each describes their initial value
-    const [localNumber, setLocalNumber] = useState(localNumberRaw)
-        // The initial value below uses a conditional operator which means: 
-            // if (condition) then (value) else (other_value)
-            // Turns into...
-            // (condition) ? (value) : (other_value)
-    const [globalNumber, setGlobalNumber] = useState(window.sessionStorage.getItem("num_global") ? window.sessionStorage.getItem("num_global") : 0);
-    const [loggedIn, setLogginIn] = useState(window.sessionStorage.getItem('current_user') ? true : false);
-    const [currentUser, setCurrentUser] = useState(window.sessionStorage.getItem('current_user'));
-
-    // Regular varaible declaration
-    const pageTitle = "About"
-    var isMobile = props.isMobile;
-
-    // 'useEffect' runs once for every render of the page
-    useEffect(() => {
-        var mainContainer = document.querySelector(".container-about");
-        var fontLarge = document.querySelectorAll(".font-round-large");
-        var fontMed = document.querySelectorAll(".font-round-medium");
-        var addButton = document.querySelectorAll(".container-add");
-
-        if(isMobile) {
-            mainContainer.style.top = "15vmin"
-
-            fontLarge.forEach(el => {
-                el.style.fontSize = "10vmin"
-            });
-
-            fontMed.forEach(el => {
-                el.style.fontSize = "5.5vmin"
-            });
-
-            addButton.forEach(el => {
-                el.style.width = "5.5vmin"
-                el.style.marginLeft = "3vmin"
-            });
+    // Importing common files used in react
+    import React, { useEffect, useRef, useState } from "react";
+    
+    // Variables declared that will persist even if page is changed
+    var localNumberRaw = 0;
+    
+    // Main function for the specific 'page'
+    function About(props) {
+        // 'Reactive' variables that will cause the page to update when their values change
+            // 'useState' at the end of each describes their initial value
+        const [localNumber, setLocalNumber] = useState(localNumberRaw)
+            // The initial value below uses a conditional operator which means: 
+                // if (condition) then (value) else (other_value)
+                // Turns into...
+                // (condition) ? (value) : (other_value)
+        const [globalNumber, setGlobalNumber] = useState(window.sessionStorage.getItem("num_global") ? window.sessionStorage.getItem("num_global") : 0);
+        const [loggedIn, setLogginIn] = useState(window.sessionStorage.getItem('current_user') ? true : false);
+        const [currentUser, setCurrentUser] = useState(JSON.parse(window.sessionStorage.getItem('current_user')));
+    
+        // Regular varaible declaration
+        const pageTitle = "About"
+        var isMobile = props.isMobile;
+    
+        // 'useEffect' runs once for every render of the page
+        useEffect(() => {
+            var mainContainer = document.querySelector(".container-about");
+            var fontLarge = document.querySelectorAll(".font-round-large");
+            var fontMed = document.querySelectorAll(".font-round-medium");
+            var addButton = document.querySelectorAll(".container-add");
+    
+            if(isMobile) {
+                mainContainer.style.top = "15vmin"
+    
+                fontLarge.forEach(el => {
+                    el.style.fontSize = "10vmin"
+                });
+    
+                fontMed.forEach(el => {
+                    el.style.fontSize = "5.5vmin"
+                });
+    
+                addButton.forEach(el => {
+                    el.style.width = "5.5vmin"
+                    el.style.marginLeft = "3vmin"
+                });
+            }
+            else {
+                mainContainer.style.top = "7vmin"
+                fontLarge.forEach(el => {
+                    el.style.fontSize = "4vmin"
+                });
+    
+                fontMed.forEach(el => {
+                    el.style.fontSize = "2.25vmin"
+                });
+    
+                addButton.forEach(el => {
+                    el.style.width = "2.5vmin"
+                    el.style.marginLeft = "1vmin"
+                });
+            }
+        });
+    
+        // Handling functions
+    
+        function handleAddButtonGlobal() {
+            setGlobalNumber(parseInt(globalNumber)+1);
+            window.sessionStorage.setItem("num_global", parseInt(globalNumber)+1);
         }
-        else {
-            mainContainer.style.top = "7vmin"
-            fontLarge.forEach(el => {
-                el.style.fontSize = "4vmin"
-            });
-
-            fontMed.forEach(el => {
-                el.style.fontSize = "2.25vmin"
-            });
-
-            addButton.forEach(el => {
-                el.style.width = "2.5vmin"
-                el.style.marginLeft = "1vmin"
-            });
-        }
-    });
-
-    // Handling functions
-
-    function handleAddButtonGlobal() {
-        setGlobalNumber(parseInt(globalNumber)+1);
-        window.sessionStorage.setItem("num_global", parseInt(globalNumber)+1);
+    
+        return (
+            // Empty root element. The return can have only one root element
+            <>
+                <div className="container-about">
+                    {/* Variables can be inserted inside of brackets as shown below */}
+                    <div className="page-title"><span className="font-round-large">{pageTitle}</span></div>
+                    <div className="test-container">
+                        <p></p>
+                        <div class="main-text-panel">
+                        <div class="main-text-body">
+                            Admin Functions:
+                            View available schedules
+                                -Cancel an existing schedule
+                            
+                        </div>
+                        </div>
+                        
+                    </div>
+                    <div className="test-container">
+                        
+                        
+                    </div>
+                    {loggedIn && <div className="user-welcome">Welcome back, <b style={{marginLeft: '0.5vmin'}}>{currentUser.User_name}</b>!</div>}
+                </div>
+            </>
+        )
     }
-
-    return (
-        // Empty root element. The return can have only one root element
-        <>
-            <div className="container-about">
-                {/* Variables can be inserted inside of brackets as shown below */}
-                <div className="page-title"><span className="font-round-large">{pageTitle}</span></div>
-                <div className="test-container">
-                    <p></p>
-                    <div class="main-text-panel">
-                    <div class="main-text-body">Homer Ford Tennis Center provides a comfortable tennis experience. Our services include equipment rental, 
-                        easy to navigate court reservation, and a text message alert system to notify you when your court is available.
-                        Choose us for your next play session or tournament, and you won't be disappointed. 
-                    </div>
-                    </div>
-                    
-                </div>
-                <div className="test-container">
-                    
-                    
-                </div>
-                {loggedIn && <div className="user-welcome">Welcome back, <b style={{marginLeft: '0.5vmin'}}>{currentUser}</b>!</div>}
-            </div>
-        </>
-    )
-}
-
-// Function must be 'exposed' to rest of the application at the end of the file as shown below.
-export default About;
+    
+    // Function must be 'exposed' to rest of the application at the end of the file as shown below.
+    export default About;
